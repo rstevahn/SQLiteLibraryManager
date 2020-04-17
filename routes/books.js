1817709336acd0161648188feb4694d131087e7e
Robert Stevahn <rstevahn@pobox.com>
2020-04-17 09:37:49 -0600

    Post Trouble
    GET routing is working and I can examine existing books and call up the form to add a new one. When I submit that form, trouble ensues.

--- routes/books.js
+++ routes/books.js
@@ -20,7 +20,31 @@ router.get('/', asyncHandler(async (req, res) => {
   res.render('books/index', { books, title: 'Library Manager' });
 }));
 
+/* GET a new book form */
+router.get('/new', (req, res) => {
+  res.render('books/new', { book: {}, title: 'New Book' });
+});
+
+/* POST a new book */
+router.post('/', asyncHandler(async (req, res) => {
+  let book;
+  console.log("req.body: " + req.body);
+  debugger;
+  try {
+    book = await Book.create(req.body);
+    res.redirect("/books/" + article.id);
+  } catch (error) {
+    if(error.name === "SequelizeValidationError") { // checking the error
+      book = await Book.build(req.body);
+      res.render("books/new", { book, errors: error.errors, title: "New Book" });
+    } else {
+      throw error; // error caught in the asyncHandler's catch block
+    }    
+  }
+}));
+
 /* GET an individual book */
+/* Important: this route must be below the other simple named routes */
 router.get('/:id', asyncHandler(async (req, res) => {
   const book = await Book.findByPk(req.params.id);
   if(book) {
@@ -29,11 +53,6 @@ router.get('/:id', asyncHandler(async (req, res) => {
     res.sendStatus(404);
   }
 }));
-
-/* GET a new book form. */
-router.get('/new', (req, res) => {
-  res.render('books/new', { book: {}, title: 'New Book' });
-});
 
 
 
